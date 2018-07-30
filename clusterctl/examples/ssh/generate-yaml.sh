3e1028face8d64cb0e8ca6a3ea5158380c4f0bd0
Alejandro Escobar <jaescobar.cell@gmail.com>
2018-07-30 10:04:30 -0700

    boostraping scripts and injections.

--- clusterctl/examples/ssh/generate-yaml.sh
+++ clusterctl/examples/ssh/generate-yaml.sh
@@ -6,12 +6,21 @@ mkdir -p ${OUTPUT_DIR}
 
 BOOTSTRAP_DIR=bootstrap_scripts
 
+# --- MACHINES ---
 MACHINE_TEMPLATE_FILE=machines.yaml.template
 MACHINE_GENERATED_FILE=${OUTPUT_DIR}/machines.yaml
+
+
+# --- CLUSTERS ---
 CLUSTER_TEMPLATE_FILE=cluster.yaml.template
 CLUSTER_GENERATED_FILE=${OUTPUT_DIR}/cluster.yaml
+
+# --- PROVIDER_CONFIG ---
 PROVIDERCOMPONENT_TEMPLATE_FILE=provider-components.yaml.template
 PROVIDERCOMPONENT_GENERATED_FILE=${OUTPUT_DIR}/provider-components.yaml
+
+PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_BOOTSTRAP=${BOOTSTRAP_DIR}/master_ubuntu_16.04.template
+PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_BOOTSTRAP=${BOOTSTRAP_DIR}/node_ubuntu_16.04.template
 
 OVERWRITE=0
 
@@ -54,6 +63,11 @@ if [ -z ${CLUSTER_PRIVATE_KEY_PLAIN+x} ]; then
     exit 1
 fi
 
+if [ -z ${CLUSTER_PASSPHRASE+x} ]; then
+    echo "using empty pass phrase to private key"
+    CLUSTER_PASSPHRASE=""
+fi
+
 # Variables that need to be base64 encoded (for secrets)
 OS=$(uname)
 if [[ "$OS" =~ "Linux" ]]; then
@@ -67,6 +81,11 @@ fi
 
 cat $PROVIDERCOMPONENT_TEMPLATE_FILE \
   | sed -e "s/\$CLUSTER_PRIVATE_KEY/$CLUSTER_PRIVATE_KEY/" \
+  | sed -e "s/\$CLUSTER_PASSPHRASE/$CLUSTER_PASSPHRASE/" \
+  | sed -e "/\$MASTER_BOOTSTRAP_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_BOOTSTRAP" \
+  | sed -e "/\$MASTER_BOOTSTRAP_SCRIPT/d" \
+  | sed -e "/\$NODE_BOOTSTRAP_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_BOOTSTRAP" \
+  | sed -e "/\$NODE_BOOTSTRAP_SCRIPT/d" \
   > $PROVIDERCOMPONENT_GENERATED_FILE
 echo "Done generating $PROVIDERCOMPONENT_GENERATED_FILE"
 
