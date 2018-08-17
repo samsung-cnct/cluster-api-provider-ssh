22523a60654c6302ae12826d5cf767c62fa59d18
Alejandro Escobar <jaescobar.cell@gmail.com>
2018-08-17 14:06:51 -0700

    Script to clean external cluster (#56)
    * added script to help dev. Should clean an external cluster.
    
    * updated the readme script to include description of the tool added
    
    * Removed readme entries.
    
    * removed extra line.

---
+++ hack/clean-external-cluster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# To be used when using either minikube or an external cluster
+# deletes resources created by using clusterctl WITH pivoting
+# If not using pivoting it may NOT be a good idea to do this
+# Since in that case machine and cluster resources may in fact
+# exist in a way that delete would and should actually delete these.
+
+machine=$1
+cluster=$2
+
+
+echo "Deleting machine object"
+kubectl patch machine ${machine} -p '{"metadata":{"finalizers": [null]}}'
+kubectl delete machine ${machine}
+
+echo "Deleting cluster object"
+kubectl patch cluster ${cluster} -p '{"metadata":{"finalizers": [null]}}'
+kubectl delete cluster ${cluster}
+
+kubectl delete configmap machine-setup -n default
+kubectl delete configmap cluster-info -n kube-public
+
+kubectl delete deployment clusterapi-controllers
+kubectl delete deployment clusterapi-apiserver
+kubectl delete statefulsets  etcd-clusterapi
