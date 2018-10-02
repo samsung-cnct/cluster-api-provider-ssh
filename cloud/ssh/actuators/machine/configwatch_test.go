89263f464a40c515d14b55e3e514ac7169a031b7
Jim Conner <snafu.x@gmail.com>
2018-10-02 02:28:02 -0700

    Refactor configwatch_test to use JSON file for testing vs the tables in the code. Should make it easier to extend in the future.

--- cloud/ssh/actuators/machine/configwatch_test.go
+++ cloud/ssh/actuators/machine/configwatch_test.go
@@ -1,64 +1,91 @@
 package machine
 
 import (
-	"testing"
+  "encoding/json"
+  "io/ioutil"
+  "os"
+  "path/filepath"
+  "testing"
+  "fmt"
 )
 
-func TestNewConfigWatch(t *testing.T) {
-	var testcases = []struct {
-		name         string
-		valid        bool
-		expectedPath string
-	}{
-		{
-			name:         "valid test file",
-			valid:        true,
-			expectedPath: "./test_files/fake_machine_setup.yaml",
-		},
-		{
-			name:         "invalid test file",
-			valid:        false,
-			expectedPath: "this/path/does/not/exist",
-		},
-	}
+var config CWTestCases
 
-	for _, tc := range testcases {
-		cw, err := NewConfigWatch(tc.expectedPath)
-		if err != nil {
-			if tc.name != "invalid test file" {
-				t.Errorf("unexpected error: for test case %s, could not create ConfigWatch", tc.name)
-			}
-		}
-		if tc.valid && cw.path != tc.expectedPath {
-			t.Errorf("error: for test case %s, ConfigWatch.path should be %s", tc.name, tc.expectedPath)
-		}
-	}
+type allTests struct {
+  tests  []string
+  test   string
+}
+
+type CWTestCases struct {
+  Testcases   []CWTestCase `json:"testcases"`
+}
+
+type CWTestCase struct {
+  Name         string `json:"name"`
+  Description  string `json:"description"`
+  Type         string `json:"type"`
+  Valid        bool   `json:"valid"`
+  ExpectedPath string `json:"expectedPath"`
+  Path         string `json:"path"`
+  Cw           ConfigWatch
+}
+
+func TestMain(m *testing.M) {
+
+  testDir         := "test-cases"
+  testJSONFile, _ := ioutil.ReadFile(filepath.Join(testDir,"configwatch.json"))
+  err             := json.Unmarshal(testJSONFile, &config)
+
+  if err != nil {
+    err.Error()
+  }
+
+  os.Exit(m.Run())
+}
+
+// TestNewConfigWatch add new tests in test-cases/configwatch.json
+func TestNewConfigWatch(t *testing.T) {
+  for _, tc := range config.Testcases {
+
+    if tc.Type == "" {
+      t.Errorf("Unexpected error: test case is missing `Type` property.")
+    }
+
+    if tc.Type == "configwatch" {
+      fmt.Printf("Test case: %v\n", tc.Name)
+
+      cw, err := NewConfigWatch(tc.ExpectedPath)
+
+      if err != nil && tc.Valid == true {
+        t.Errorf("Unexpected error: could not create ConfigWatch for test ")
+        t.Errorf("case: '%s' error: '%s'", tc.Name, err)
+      }
+
+      if tc.Valid && cw.path != tc.ExpectedPath {
+        t.Errorf("Error: wrong path for ConfigWatch.path for test case '%s'. ", tc.Name)
+        t.Errorf("Should be '%s', but was '%s'.", tc.ExpectedPath, cw.path)
+      }
+    }
+  }
 }
 
 func TestGetMachineSetupConfig(t *testing.T) {
-	var testcases = []struct {
-		name  string
-		valid bool
-		cw    ConfigWatch
-	}{
-		{
-			name:  "valid test config path",
-			valid: true,
-			cw:    ConfigWatch{path: "./test_files/fake_machine_setup.yaml"},
-		},
-		{
-			name:  "valid test config path",
-			valid: false,
-			cw:    ConfigWatch{path: "/no/such/path"},
-		},
-	}
-	for _, tc := range testcases {
-		mc, err := tc.cw.GetMachineSetupConfig()
-		if err != nil && tc.valid {
-			t.Errorf("unexpected error: for test case %s, could not get MachineSetupConfig", tc.name)
-		}
-		if !tc.valid && mc != nil {
-			t.Errorf("unexpected error: for test case %s, expected MachineSetupConfig to be nil", tc.name)
-		}
-	}
+  for _, tc := range config.Testcases {
+
+    machine, err := tc.Cw.GetMachineSetupConfig()
+    tc.Cw         = ConfigWatch{path: tc.Path}
+
+    if tc.Type == "configmachine" {
+      fmt.Printf("Test case: %v\n", tc.Name)
+
+      if err != nil && tc.Valid == true {
+        t.Errorf("Unexpected error: could not get MachineSetupConfig:")
+        t.Errorf("case: '%s' error: '%s'", tc.Name, err)
+      }
+
+      if !tc.Valid && machine != nil {
+        t.Errorf("Unexpected error: expected MachineSetupConfig to be nil for test case %s.", tc.Name)
+      }
+    }
+  }
 }
