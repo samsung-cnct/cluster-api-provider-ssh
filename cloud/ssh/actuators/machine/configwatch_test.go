0ad51a2caac78abadb23533f8969c5a701f94d97
Jim Conner <snafu.x@gmail.com>
2018-10-03 00:16:19 -0700

    Updated configwatch_test with being more consistent with standards. Some minor bugfixes.

--- cloud/ssh/actuators/machine/configwatch_test.go
+++ cloud/ssh/actuators/machine/configwatch_test.go
@@ -6,25 +6,18 @@ import "testing"
 func TestNewConfigWatch(t *testing.T) {
   var testcases = []struct {
     name         string
-    description  string
-    testtype     string
     valid        bool
     path         string
-    cw           ConfigWatch
   }{
       {
-        name:         "valid test file",
-        testtype:     "configwatch",
-        description:  "Valid test file - test if valid file exists.",
-        valid:         true,
-        path:         "./test-files/fake_machine_setup.yaml",
+        name:   "valid test file",
+        valid:   true,
+        path:   "./test-files/fake_machine_setup.yaml",
       },
       {
-        name:         "invalid test file",
-        testtype:     "configwatch",
-        description:  "Invalid test file - test if invalid file exists.",
-        valid:         false,
-        path:         "this/path/does/not/exist",
+        name:   "invalid test file",
+        valid:   false,
+        path:   "this/path/does/not/exist",
       },
     }
 
@@ -33,7 +26,7 @@ func TestNewConfigWatch(t *testing.T) {
     cw, err := NewConfigWatch(tc.path)
 
     if err != nil && tc.valid {
-      t.Errorf("Unexpected error: could not create ConfigWatch for test ")
+      t.Error("Unexpected error: could not create ConfigWatch for test ")
       t.Errorf("case: '%s' error: '%s'", tc.name, err)
     }
 
@@ -47,40 +40,32 @@ func TestNewConfigWatch(t *testing.T) {
 func TestGetMachineSetupConfig(t *testing.T) {
   var testcases = []struct {
     name         string
-    description  string
-    testtype     string
     valid        bool
-    path         string
     cw           ConfigWatch
   }{
       {
-        name:         "valid test config path",
-        testtype:     "configmachine",
-        valid:         true,
-        path:         "./test_files/fake_machine_setup.yaml",
+        name:   "valid test config path",
+        valid:   true,
+        cw:      ConfigWatch{path: "./test-files/fake_machine_setup.yaml"},
       },
       {
-        name:         "invalid test config path",
-        testtype:     "configmachine",
-        valid:         false,
-        path:         "/no/such/path",
+        name:   "invalid test config path",
+        valid:   false,
+        cw:      ConfigWatch{path: "/no/such/path"},
       },
     }
 
   for _, tc := range testcases {
 
-    tc.cw         = ConfigWatch{path: tc.path}
     machine, err := tc.cw.GetMachineSetupConfig()
 
-    if tc.testtype == "configmachine" {
-      if err != nil && tc.valid {
-        t.Errorf("Unexpected error: could not get MachineSetupConfig:")
-        t.Errorf("case: '%s' error: '%s'", tc.name, err)
-      }
+    if err != nil && tc.valid {
+      t.Error("Unexpected error: could not get MachineSetupConfig:")
+      t.Errorf("case: '%s' error: '%s'", tc.name, err)
+    }
 
-      if !tc.valid && machine != nil {
-        t.Errorf("Unexpected error: expected MachineSetupConfig to be nil for test case %s.", tc.name)
-      }
+    if !tc.valid && machine != nil {
+      t.Errorf("Unexpected error: expected MachineSetupConfig to be nil for test case %s.", tc.name)
     }
   }
 }
