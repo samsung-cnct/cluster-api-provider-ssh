295899a75e23d8b238218594aa751c09274a0cb7
Jim Conner <snafu.x@gmail.com>
2018-10-02 23:17:27 -0700

    test-cases/configwatch.json

--- cloud/ssh/actuators/machine/README.md
+++ cloud/ssh/actuators/machine/README.md
@@ -1,13 +1,21 @@
-##Tests
+# Config Watch
+
+## Tests
+
+### Purpose
+
+The purpose of the configwatch tests is essentially to validate the ability to
+find and open files.The `configwatch_test.go` source file requires a single
+file in `test-cases/configwatch.json` exists.
+
 ----------
 
-The `configwatch_test.go` source file requires a single file in `test-cases/configwatch.json` exists. The
-structure of the JSON should be an object called `testcases` followed by an array of keyed indexes with the
-following names, "name", "type", "description", "path" (see example below). The "type" can be either
-"configwatch" or "configmachine".  Anything else is currently ignored. If `type` is missing then an error
-will be thrown. Both tests are technically verifying paths. However, one verifies the path independently
-of object creation and the other verifies the path for the `ConfigWatch` object.
+### JSON Structure
 
-Purpose
------------
-The purpose of the configwatch tests is essentially to validate the ability to find and open files.
+The structure of the JSON should be an object called `testcases` followed by
+an array of keyed indexes with the following names, "name", "type",
+"description", "path" (see example below). The "type" can be either
+"configwatch" or "configmachine".  Anything else is currently ignored. If
+`type` is missing then an error will be thrown. Both tests are technically
+verifying paths. However, one verifies the path independently of object
+creation and the other verifies the path for the `ConfigWatch` object.
--- cloud/ssh/actuators/machine/configwatch_test.go
+++ cloud/ssh/actuators/machine/configwatch_test.go
@@ -1,85 +1,88 @@
 package machine
 
 import (
-  "encoding/json"
-  "io/ioutil"
-  "os"
-  "path/filepath"
   "testing"
   "fmt"
-  "log"
 )
-
-var config CWTestCases
-
-type CWTestCases struct {
-  Testcases   []CWTestCase `json:"testcases"`
-}
-
-type CWTestCase struct {
-  Name         string `json:"name"`
-  Description  string `json:"description"`
-  Type         string `json:"type"`
-  Valid        bool   `json:"valid"`
-  Path         string `json:"path"`
-  Cw           ConfigWatch
-}
-
-func TestMain(m *testing.M) {
-
-  testDir         := "test-cases"
-  testJSONFile, _ := ioutil.ReadFile(filepath.Join(testDir,"configwatch.json"))
-  err             := json.Unmarshal(testJSONFile, &config)
-
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  os.Exit(m.Run())
-}
 
 // TestNewConfigWatch add new tests in test-cases/configwatch.json
 func TestNewConfigWatch(t *testing.T) {
-  for _, tc := range config.Testcases {
-
-    if tc.Type == "" {
-      t.Errorf("Unexpected error: test case is missing `Type` property.")
+  var testcases = []struct {
+    name         string
+    description  string
+    testtype     string
+    valid        bool
+    path         string
+    cw           ConfigWatch
+  }{
+      {
+        name:         "valid test file",
+        testtype:     "configwatch",
+        description:  "Valid test file - test if valid file exists.",
+        valid:         true,
+        path:         "./test-files/fake_machine_setup.yaml",
+      },
+      {
+        name:         "invalid test file",
+        testtype:     "configwatch",
+        description:  "Invalid test file - test if invalid file exists.",
+        valid:         false,
+        path:         "this/path/does/not/exist",
+      },
     }
 
-    if tc.Type == "configwatch" {
-      fmt.Printf("Test case: %v\n", tc.Name)
+  for _, tc := range testcases {
 
-      cw, err := NewConfigWatch(tc.Path)
+    cw, err := NewConfigWatch(tc.path)
 
-      if err != nil && tc.Valid {
-        t.Errorf("Unexpected error: could not create ConfigWatch for test ")
-        t.Errorf("case: '%s' error: '%s'", tc.Name, err)
-      }
+    if err != nil && tc.valid {
+      t.Errorf("Unexpected error: could not create ConfigWatch for test ")
+      t.Errorf("case: '%s' error: '%s'", tc.name, err)
+    }
 
-      if tc.Valid && cw.path != tc.Path {
-        t.Errorf("Error: wrong path for ConfigWatch.path for test case '%s'. ", tc.Name)
-        t.Errorf("Should be '%s', but was '%s'.", tc.Path, cw.path)
-      }
+    if tc.valid && cw.path != tc.path {
+      t.Errorf("Error: wrong path for ConfigWatch.path for test case '%s'. ", tc.name)
+      t.Errorf("Should be '%s', but was '%s'.", tc.path, cw.path)
     }
   }
 }
 
 func TestGetMachineSetupConfig(t *testing.T) {
-  for _, tc := range config.Testcases {
+  var testcases = []struct {
+    name         string
+    description  string
+    testtype     string
+    valid        bool
+    path         string
+    cw           ConfigWatch
+  }{
+      {
+        name:         "valid test config path",
+        testtype:     "configmachine",
+        valid:         true,
+        path:         "./test_files/fake_machine_setup.yaml",
+      },
+      {
+        name:         "invalid test config path",
+        testtype:     "configmachine",
+        valid:         false,
+        path:         "/no/such/path",
+      },
+    }
 
-    tc.Cw         = ConfigWatch{path: tc.Path}
-    machine, err := tc.Cw.GetMachineSetupConfig()
+  for _, tc := range testcases {
 
-    if tc.Type == "configmachine" {
-      fmt.Printf("Test case: %v\n", tc.Name)
+    tc.cw         = ConfigWatch{path: tc.path}
+    machine, err := tc.cw.GetMachineSetupConfig()
 
-      if err != nil && tc.Valid {
+    if tc.testtype == "configmachine" {
+      if err != nil && tc.valid {
         t.Errorf("Unexpected error: could not get MachineSetupConfig:")
-        t.Errorf("case: '%s' error: '%s'", tc.Name, err)
+        t.Errorf("case: '%s' error: '%s'", tc.name, err)
       }
 
-      if !tc.Valid && machine != nil {
-        t.Errorf("Unexpected error: expected MachineSetupConfig to be nil for test case %s.", tc.Name)
+      if !tc.valid && machine != nil {
+        t.Errorf("Unexpected error: expected MachineSetupConfig to be nil for test case %s.", tc.name)
       }
     }
   }
