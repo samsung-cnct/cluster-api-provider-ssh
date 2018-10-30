9aab2b6370b2fc51a9f4ce182ce29516b1ca7ffe
Jim Conner <snafu.x@gmail.com>
2018-10-30 00:00:25 -0700

    Added bootstrap templates for CentOS. They still need vetting. Fixed replaced use of sed for envsubst which is safer and designed as a templating method. Functionalized the generate script for better readability. Added functionality for machine.yaml to actually be processed so that OS_TYPE and KUBELET_VERSION are injected. Fixes some bugs. Created common_functions.template which will allow easier editing of templates vs having to potentially fix code in several templates.

--- clusterctl/examples/ssh/generate-yaml.sh
+++ clusterctl/examples/ssh/generate-yaml.sh
@@ -4,118 +4,208 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# All configuration is done using environment variables.
-if [ "$#" -ne 0 ]; then
-    echo "Illegal number of parameters"
-fi
+inject_functions()
+{
+  local FUNCTIONS tmpl
 
-OUTPUT_DIR=out
-mkdir -p ${OUTPUT_DIR}
+  FUNCTIONS=$(< "$bootstrap_dir/common_functions.template")
+  tmpl=$1
 
-BOOTSTRAP_DIR=bootstrap_scripts
+  export FUNCTIONS
 
-OS_TYPE=${OS_TYPE:-ubuntu}
+  [[ -z "${tmpl}" ]] && \
+    {
+      echo >&2 "Usage: preprocess_template(): caller must provide template to process."
+      return 45
+    }
 
-# --- MACHINES ---
-MACHINE_TEMPLATE_FILE=machines.yaml.template
-MACHINE_GENERATED_FILE=${OUTPUT_DIR}/machines.yaml
+  # shellcheck disable=SC2016
+  envsubst '${FUNCTIONS}' < "$tmpl"
+}
 
+runpath()
+{
+  pushd . > /dev/null
+  SCRIPT_PATH="${BASH_SOURCE[0]}"
+  if ([ -h "${SCRIPT_PATH}" ]); then
+    while([ -h "${SCRIPT_PATH}" ]); do
+      cd "$(dirname "$SCRIPT_PATH")";
+      SCRIPT_PATH=$(readlink "${SCRIPT_PATH}");
+    done
+  fi
+  cd "$(dirname "${SCRIPT_PATH}")" > /dev/null
+  pwd
+  popd  > /dev/null
+}
 
-# --- CLUSTERS ---
-CLUSTER_TEMPLATE_FILE=cluster.yaml.template
-CLUSTER_GENERATED_FILE=${OUTPUT_DIR}/cluster.yaml
+generate_yaml()
+{
+  local bootstrap_dir machine_template_file machine_generated_file cluster_template_file
+  local cluster_generated_file providercomponent_template_file providercomponent_generated_file
+  local master_upgrade_script master_teardown_script node_upgrade_script node_teardown_script
 
-# --- PROVIDER_CONFIG ---
-PROVIDERCOMPONENT_TEMPLATE_FILE=provider-components.yaml.template
-PROVIDERCOMPONENT_GENERATED_FILE=${OUTPUT_DIR}/provider-components.yaml
+  if ! mkdir -p "${OUTPUT_DIR}" 2>/dev/null; then
+    echo >&2 "Unable to mkdir $OUTPUT_DIR"
+    return 12
+  fi
 
-if [ $OS_TYPE == "ubuntu" ]; then
-    PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_BOOTSTRAP=${BOOTSTRAP_DIR}/master_bootstrap_ubuntu_16.04.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_BOOTSTRAP=${BOOTSTRAP_DIR}/node_bootstrap_ubuntu_16.04.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_TEARDOWN=${BOOTSTRAP_DIR}/master_teardown_ubuntu_16.04.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_TEARDOWN=${BOOTSTRAP_DIR}/node_teardown_ubuntu_16.04.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_UPGRADE=${BOOTSTRAP_DIR}/master_upgrade_ubuntu_16.04.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_UPGRADE=${BOOTSTRAP_DIR}/node_upgrade_ubuntu_16.04.template
-else 
-    PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_BOOTSTRAP=${BOOTSTRAP_DIR}/master_bootstrap_air_gapped_centos_7.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_BOOTSTRAP=${BOOTSTRAP_DIR}/node_bootstrap_air_gapped_centos_7.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_TEARDOWN=${BOOTSTRAP_DIR}/master_teardown_centos_7.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_TEARDOWN=${BOOTSTRAP_DIR}/node_teardown_centos_7.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_UPGRADE=${BOOTSTRAP_DIR}/master_upgrade_centos_7.template
-    PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_UPGRADE=${BOOTSTRAP_DIR}/node_upgrade_centos_7.template
-fi
+  bootstrap_dir=bootstrap_scripts
 
-OVERWRITE=0
+  # --- MACHINES ---
+  machine_template_file="$BASEDIR/machines.yaml.template"
+  machine_generated_file=${OUTPUT_DIR}/machines.yaml
 
-SCRIPT=$(basename $0)
-while test $# -gt 0; do
-        case "$1" in
-          -h|--help)
-            echo "$SCRIPT - generates input yaml files for Cluster API on openstack"
-            echo " "
-            echo "$SCRIPT [options]"
-            echo " "
-            echo "options:"
-            echo "-h, --help                show brief help"
-            echo "-f, --force-overwrite     if file to be generated already exists, force script to overwrite it"
-            exit 0
-            ;;
-          -f)
-            OVERWRITE=1
-            shift
-            ;;
-          --force-overwrite)
-            OVERWRITE=1
-            shift
-            ;;
-          *)
-            break
-            ;;
-        esac
-done
+  # --- CLUSTERS ---
+  cluster_template_file="$BASEDIR/cluster.yaml.template"
+  cluster_generated_file=${OUTPUT_DIR}/cluster.yaml
 
-if [ $OVERWRITE -ne 1 ] && [ -f $PROVIDERCOMPONENT_GENERATED_FILE ]; then
-  echo "File $PROVIDERCOMPONENT_GENERATED_FILE already exists. Delete it manually before running this script."
-  exit 1
-fi
+  # --- PROVIDER_CONFIG ---
+  providercomponent_template_file="$BASEDIR/provider-components.yaml.template"
+  providercomponent_generated_file=${OUTPUT_DIR}/provider-components.yaml
 
-# TODO Fill out the generation pieces as we need them.
+  if [[ $OVERWRITE -ne 1 ]] && [[ -f $providercomponent_generated_file ]]; then
+    echo >&1 "File $providercomponent_generated_file already exists. Delete it manually before running this script."
+    return 25
+  fi
 
-if [ -z ${CLUSTER_PRIVATE_KEY+x} ]; then
-    echo "Please generate a valid base64 encoded cluster private key and export the key file contents to CLUSTER_PRIVATE_KEY."
-    exit 1
-fi
+  # This sorely needs optimization. The file naming convention and usage here is not scalable.
+  if [[ "${OS_TYPE}" == "ubuntu" ]]; then
+    MASTER_BOOTSTRAP_SCRIPT="$(< ${bootstrap_dir}/master_bootstrap_ubuntu_16.04.template)"
+    MASTER_TEARDOWN_SCRIPT="$(< ${bootstrap_dir}/master_teardown_ubuntu_16.04.template)"
+    MASTER_UPGRADE_SCRIPT="$(< ${bootstrap_dir}/master_upgrade_ubuntu_16.04.template)"
 
-if [ -z ${CLUSTER_PASSPHRASE+x} ]; then
-    echo "using empty cluster pass phrase to private key"
-    CLUSTER_PASSPHRASE='""'
-fi
+    NODE_BOOTSTRAP_SCRIPT="$(< ${bootstrap_dir}/node_bootstrap_ubuntu_16.04.template)"
+    NODE_TEARDOWN_SCRIPT="$(< ${bootstrap_dir}/node_teardown_ubuntu_16.04.template)"
+    NODE_UPGRADE_SCRIPT="$(< ${bootstrap_dir}/node_upgrade_ubuntu_16.04.template)"
+  else
+    if [[ ${SDS_CASE} == 0 ]]; then
+      MASTER_BOOTSTRAP_SCRIPT="$(< ${bootstrap_dir}/master_bootstrap_aws_centos_7.template)"
 
-cat $PROVIDERCOMPONENT_TEMPLATE_FILE \
-  | sed -e "s/\$CLUSTER_PRIVATE_KEY/$CLUSTER_PRIVATE_KEY/" \
-  | sed -e "s/\$CLUSTER_PASSPHRASE/$CLUSTER_PASSPHRASE/" \
-  | sed -e "/\$MASTER_BOOTSTRAP_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_BOOTSTRAP" \
-  | sed -e "/\$MASTER_BOOTSTRAP_SCRIPT/d" \
-  | sed -e "/\$NODE_BOOTSTRAP_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_BOOTSTRAP" \
-  | sed -e "/\$NODE_BOOTSTRAP_SCRIPT/d" \
-  | sed -e "/\$MASTER_TEARDOWN_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_TEARDOWN" \
-  | sed -e "/\$MASTER_TEARDOWN_SCRIPT/d" \
-  | sed -e "/\$NODE_TEARDOWN_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_TEARDOWN" \
-  | sed -e "/\$NODE_TEARDOWN_SCRIPT/d" \
-  | sed -e "/\$MASTER_UPGRADE_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_MASTER_UPGRADE" \
-  | sed -e "/\$MASTER_UPGRADE_SCRIPT/d" \
-  | sed -e "/\$NODE_UPGRADE_SCRIPT/r $PROVIDERCOMPONENT_TEMPLATE_FILE_NODE_UPGRADE" \
-  | sed -e "/\$NODE_UPGRADE_SCRIPT/d" \
-  > $PROVIDERCOMPONENT_GENERATED_FILE
-echo "Done generating $PROVIDERCOMPONENT_GENERATED_FILE"
+      NODE_BOOTSTRAP_SCRIPT="$(< ${bootstrap_dir}/node_bootstrap_aws_centos_7.template)"
+    else
+      MASTER_BOOTSTRAP_SCRIPT="$(< ${bootstrap_dir}/master_bootstrap_air_gapped_centos_7.template)"
 
-cat $MACHINE_TEMPLATE_FILE \
-  > $MACHINE_GENERATED_FILE
-echo "Done generating $MACHINE_GENERATED_FILE"
+      NODE_BOOTSTRAP_SCRIPT="$(< ${bootstrap_dir}/node_bootstrap_air_gapped_centos_7.template)"
+    fi
 
-cat $CLUSTER_TEMPLATE_FILE \
-  > $CLUSTER_GENERATED_FILE
-echo "Done generating $CLUSTER_GENERATED_FILE"
+    MASTER_TEARDOWN_SCRIPT="$(< ${bootstrap_dir}/master_teardown_centos_7.template)"
+    MASTER_UPGRADE_SCRIPT="$(< ${bootstrap_dir}/master_upgrade_centos_7.template)"
 
-echo "You will still need to _edit_ the cluster.yaml and machines.yaml! See the README.md for details."
+    NODE_TEARDOWN_SCRIPT="$(< ${bootstrap_dir}/node_teardown_centos_7.template)"
+    NODE_UPGRADE_SCRIPT="$(< ${bootstrap_dir}/node_upgrade_centos_7.template)"
+  fi
 
+  # prepend common functions to template script
+  FUNCTIONS=$(< "$bootstrap_dir/common_functions.template")
+
+  export CLUSTER_PRIVATE_KEY CLUSTER_PASSPHRASE MASTER_BOOTSTRAP_SCRIPT \
+         NODE_BOOTSTRAP_SCRIPT MASTER_TEARDOWN_SCRIPT NODE_TEARDOWN_SCRIPT MASTER_UPGRADE_SCRIPT \
+         NODE_UPGRADE_SCRIPT FUNCTIONS OS_TYPE KUBELET_VERSION
+
+  # shellcheck disable=SC2016
+  envsubst '$CLUSTER_PRIVATE_KEY $CLUSTER_PASSPHRASE $MASTER_BOOTSTRAP_SCRIPT
+            $NODE_BOOTSTRAP_SCRIPT  $MASTER_TEARDOWN_SCRIPT $NODE_TEARDOWN_SCRIPT
+            $MASTER_UPGRADE_SCRIPT $NODE_UPGRADE_SCRIPT $KUBELET_VERSION $OS_TYPE' \
+           < "$providercomponent_template_file" > "$providercomponent_generated_file-tmp"
+
+  if inject_functions "$providercomponent_generated_file-tmp" > "$providercomponent_generated_file"; then
+    rm "$providercomponent_generated_file-tmp" 2>/dev/null
+  fi
+
+  echo "Done generating $providercomponent_generated_file"
+
+  cat "$machine_template_file" \
+    > "$machine_generated_file"
+  echo "Done generating $machine_generated_file"
+
+  cat "$cluster_template_file" \
+    > "$cluster_generated_file"
+  echo "Done generating $cluster_generated_file"
+
+  echo "You will still need to _edit_ the cluster.yaml and machines.yaml manifests! See the README.md for details."
+}
+
+main()
+{
+  SCRIPT=$(basename "$0")
+  OS_TYPE=${OS_TYPE:-ubuntu}
+  BASEDIR="$(runpath)"
+  OUTPUT_DIR="$BASEDIR/out"
+  KUBELET_VERSION=${KUBELET_VERSION:-1.10.6}
+  OVERWRITE=0
+  SDS_CASE=${SDS_CASE:-0}
+
+  while test $# -gt 0; do
+    case "$1" in
+      -h|--help)
+  # shellcheck disable=SC2140
+        echo """
+  $SCRIPT - generates input yaml files for Cluster API on openstack. Some environment
+        variables are needed set for $SCRIPT to properly function:
+
+        CLUSTER_PRIVATE_KEY   : base64 encoded private key used when make_cluster was run.
+        OS_TYPE               : One of "ubuntu" or "centos" -- defaults to "ubuntu"
+        CLUSTER_PASSPHRASE    : Only used if CLUSTER_PRIVATE_KEY was generated using a passphrase.
+        SDS_CASE              : 0 or 1 -- use 0 if creating a cluster in AWS or MaaS. Use 1 if
+                                creating a cluster in SDS Cloud -- defaults to 0.
+        KUBELET_VERSION       : e.g. 1.10.6 -- do not prepend a 'v' in front of it -- currently defaults to 1.10.6
+
+  $SCRIPT [options]
+
+  options:
+  -h, --help                show brief help
+  -f, --force-overwrite     if file to be generated already exists, force script to overwrite it
+
+  Completed manifests are placed in '$OUTPUT_DIR'
+
+        """
+        exit 0
+        ;;
+      -f|--force-overwrite)
+        OVERWRITE=1
+        shift
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+
+  # TODO Fill out the generation pieces as we need them.
+
+  if [[ -z ${CLUSTER_PRIVATE_KEY+x} ]]; then
+      echo "Please generate a valid base64 encoded cluster private key and export the key file contents to CLUSTER_PRIVATE_KEY."
+      exit 1
+  fi
+
+  if [[ -z "${CLUSTER_PASSPHRASE+x}" ]]; then
+      echo "Using empty cluster pass phrase to private key"
+      CLUSTER_PASSPHRASE='""'
+  fi
+
+  if [[ "${OS_TYPE}" =~ (centos|ubuntu) ]]; then
+    echo "OS Type set for valid type: $OS_TYPE."
+  else
+    echo >&2 "Invalid parameter for \$OS_TYPE: '$OS_TYPE'. Must be either 'ubuntu' or 'centos'"
+    exit 15
+  fi
+
+  if [[ "${SDS_CASE}" =~ [01] ]]; then
+    case ${SDS_CASE} in
+      0) echo "Creating templates for non-SDS Cloud environment.";;
+      1) echo "Creating templates for SDS Cloud environment.";;
+      *) echo "Invalid option: for SDS_CASE: ${SDS_CASE}";
+        exit 30
+      ;;
+    esac
+  else
+    echo >&2 "Invalid parameter for \$SDS_CASE: '$SDS_CASE'. Must be either '1' or '0'."
+    exit 20
+  fi
+
+  if ! generate_yaml; then
+    exit $?
+  fi
+}
+
+main "$@"
